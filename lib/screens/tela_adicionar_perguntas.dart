65292a91a140756bdb0d8f0e981daf1fc009c5f6
LucenaNeto <cneto808@gmail.com>
2024-10-18 18:29:16 -0300

    atualizações nos campos onde o usuario tem interação

--- lib/screens/tela_adicionar_perguntas.dart
+++ lib/screens/tela_adicionar_perguntas.dart
@@ -1,3 +1,4 @@
+//import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:audi_mag/db_helper.dart';
 
@@ -15,11 +16,21 @@ class _TelaAdicionarPerguntasState extends State<TelaAdicionarPerguntas> {
   String novaPergunta = '';
   String observacao = '';
 
+  // controladores de campo
+
+  TextEditingController perguntaController = TextEditingController();
+  TextEditingController observacaoController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Adicionar Perguntas'),
+        title: Text(
+          'Adicionar Perguntas',
+          style: TextStyle(fontSize: 24, color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(132, 10, 66, 32),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -28,7 +39,7 @@ class _TelaAdicionarPerguntasState extends State<TelaAdicionarPerguntas> {
             Card(
               elevation: 4,
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.circular(20),
               ),
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
@@ -36,6 +47,7 @@ class _TelaAdicionarPerguntasState extends State<TelaAdicionarPerguntas> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     TextFormField(
+                      controller: perguntaController,
                       decoration: InputDecoration(labelText: 'Pergunta'),
                       validator: (value) {
                         if (value == null || value.isEmpty) {
@@ -43,17 +55,12 @@ class _TelaAdicionarPerguntasState extends State<TelaAdicionarPerguntas> {
                         }
                         return null;
                       },
-                      onChanged: (value) {
-                        novaPergunta = value;
-                      },
                     ),
                     SizedBox(height: 10),
                     TextFormField(
+                      controller: observacaoController,
                       decoration:
                           InputDecoration(labelText: 'Observação (opcional)'),
-                      onChanged: (value) {
-                        observacao = value;
-                      },
                     ),
                   ],
                 ),
@@ -62,21 +69,21 @@ class _TelaAdicionarPerguntasState extends State<TelaAdicionarPerguntas> {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () async {
-                if (novaPergunta.isNotEmpty) {
+                if (perguntaController.text.isNotEmpty) {
                   await DBHelper().salvarPergunta(
                     widget.auditoriaId,
-                    novaPergunta,
-                    observacao,
+                    perguntaController.text,
+                    observacaoController.text,
                     null, // Removido campo de imagem
                   );
                   setState(() {
                     _perguntas.add({
-                      'pergunta': novaPergunta,
-                      'observacao': observacao,
+                      'pergunta': perguntaController.text,
+                      'observacao': observacaoController.text,
                       'resposta': null,
                     });
-                    novaPergunta = '';
-                    observacao = '';
+                    perguntaController.clear();
+                    observacaoController.clear();
                   });
                 }
               },
@@ -99,19 +106,24 @@ class _TelaAdicionarPerguntasState extends State<TelaAdicionarPerguntas> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            _perguntas[index]['observacao'] ?? 'Sem observação',
+                            _perguntas[index]['pergunta'] ?? 'Sem pergunta',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          // Exibe a observação se houver
+                          Text(
+                            _perguntas[index]['observacao']?.isNotEmpty == true
+                                ? _perguntas[index]['observacao']
+                                : 'Sem Observação',
                             style: TextStyle(color: Colors.grey[700]),
                           ),
                           SizedBox(height: 10),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.end,
                             children: [
-                              IconButton(
-                                icon: Icon(Icons.edit),
-                                onPressed: () {
-                                  // Função para editar - ainda falta ser implementada
-                                },
-                              ),
                               IconButton(
                                 icon: Icon(Icons.delete),
                                 onPressed: () {
@@ -134,4 +146,14 @@ class _TelaAdicionarPerguntasState extends State<TelaAdicionarPerguntas> {
       ),
     );
   }
+
+  @override
+  void dispose() {
+    //libertar os controladores, para evitar vazar memoria
+    perguntaController.dispose();
+    observacaoController.dispose();
+    super.dispose();
+  }
+
+//futuramente vou colocar as funções aqui caso nas minhas pesquisas eu ver que seja interessante
 }
